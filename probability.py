ce91adb82bae2027109f137fd93c290c1b48e1e8
Mikey Jay <65405097+mikey-jay@users.noreply.github.com>
2023-07-04 15:36:31 -0400

    updated base probabilities and test code, round floats to 2 places

--- probability.py
+++ probability.py
@@ -1,13 +1,16 @@
 import itertools
 
 BASE_PROBABILITIES = {
-        'common': [0.2128, 0.0992, 0.024, 0.0062, 0.0014, 0.0003],
-        'uncommon': [0.3737, 0.2512, 0.0691, 0.0186, 0.004, 0.001],
-        'rare': [0.1568, 0.4951, 0.2457, 0.0765, 0.0175, 0.0042],
-        'legendary': [0.0006, 0.1788, 0.4767, 0.2584, 0.0685, 0.017],
-        'mythical': [0, 0.0003, 0.1367, 0.5191, 0.2661, 0.0778],
-        'godlike': [0, 0, 0, 0.1117, 0.5444, 0.3439]
+        'common': [0.22, 0.06, 0, 0, 0, 0],
+        'uncommon': [0.22, 0.15, 0.04, 0, 0, 0],
+        'rare': [0.11, 0.25, 0.17, 0.04, 0, 0],
+        'legendary': [0.05, 0.15, 0.3, 0.18, 0.03, 0],
+        'mythical': [0, 0.08, 0.18, 0.38, 0.2, 0.02],
+        'godlike': [0, 0, 0, 0.17, 0.54, 0.29]
 }
+
+def round_list_of_floats(list_of_floats, precision):
+    return [round(x, precision) for x in list_of_floats]
 
 def get_probabilities_based_on_num_prizes(geode_rarity, num_prizes_available):
     
@@ -24,16 +27,16 @@ def get_probabilities_based_on_num_prizes(geode_rarity, num_prizes_available):
     if num_prizes_available[0] == 0:
         probabilities[0] = 0
 
-    return probabilities
+    return round_list_of_floats(probabilities, 2) # only rounding here for the sake of the test code
 
 # Test code
 
-assert get_probabilities_based_on_num_prizes('godlike', [1,1,1,1,1,1]) == [0, 0, 0, 0.1117, 0.5444, 0.3439] # full prize pool
-assert get_probabilities_based_on_num_prizes('godlike', [1,1,1,1,1,0]) == [0, 0, 0, 0.1117, 0.8883, 0] # missing godlike prizes
-assert get_probabilities_based_on_num_prizes('godlike', [1,0,0,0,0,0]) == [1, 0, 0, 0, 0, 0] # godlik geode with only common prizes
-assert get_probabilities_based_on_num_prizes('common', [0,0,0,0,0,1]) == [0, 0, 0, 0, 0, 0.0003] # common geode with only godlike prizes
+assert get_probabilities_based_on_num_prizes('godlike', [1,1,1,1,1,1]) == [0, 0, 0, 0.17, 0.54, 0.29] # full prize pool
+assert get_probabilities_based_on_num_prizes('godlike', [1,1,1,1,1,0]) == [0, 0, 0, 0.17, 0.83, 0] # missing godlike prizes
+assert get_probabilities_based_on_num_prizes('godlike', [1,0,0,0,0,0]) == [1, 0, 0, 0, 0, 0] # godlike geode with only common prizes
+assert get_probabilities_based_on_num_prizes('common', [0,0,0,0,0,1]) == [0, 0, 0, 0, 0, 0] # common geode with only godlike prizes
 assert get_probabilities_based_on_num_prizes('godlike', [0,0,0,0,0,0]) == [0, 0, 0, 0, 0, 0] # empty prize pool
-assert get_probabilities_based_on_num_prizes('rare', [0,0,0,1,0,0]) == [0, 0, 0, 0.0982, 0, 0] # rare geode with only legendary prizes
+assert get_probabilities_based_on_num_prizes('rare', [0,0,0,1,0,0]) == [0, 0, 0, 0.04, 0, 0] # rare geode with only legendary prizes
 
 # Iterate over all combinations of geode_rarity and num_prizes_available - sum of probabilities should always be the same as the sum of the base probabilities
 
@@ -43,6 +46,6 @@ for geode_rarity, probabilities in BASE_PROBABILITIES.items():
     for num_prizes_available in supply_combinations:
         result = get_probabilities_based_on_num_prizes(geode_rarity, num_prizes_available)
         if sum(num_prizes_available) > 0:
-            assert sum(result) == sum(probabilities)
+            assert round(sum(result),2) == round(sum(probabilities),2)
         else:
             assert sum(result) == 0
